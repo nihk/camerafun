c30b282169aa66f7826c432377f56e3ab85f2128
Nick Rose <nihk@github.com>
2021-05-26 18:16:52 -0400

    Reconcile process recreation with permissions requesting

--- app/src/main/java/nick/camerafun/PermissionsFragment.kt
+++ app/src/main/java/nick/camerafun/PermissionsFragment.kt
@@ -7,29 +7,26 @@ import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
-import kotlinx.coroutines.launch
 
 class PermissionsFragment : Fragment() {
+    private val launcher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+        if (permissions.all { it.value }) {
+            navigateToCamera()
+        } else {
+            Toast.makeText(requireContext(), "Permissions were not granted :(", Toast.LENGTH_LONG)
+                .show()
+            requireActivity().finish()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (hasPermissions()) {
             navigateToCamera()
         } else {
-            lifecycleScope.launch {
-                val permissions = activityResult(
-                    REQUESTED_PERMISSIONS,
-                    ActivityResultContracts.RequestMultiplePermissions()
-                )
-                if (permissions.all { it.value }) {
-                    navigateToCamera()
-                } else {
-                    Toast.makeText(requireContext(), "Permissions were not granted :(", Toast.LENGTH_LONG)
-                        .show()
-                    requireActivity().finish()
-                }
+            if (savedInstanceState == null) {
+                launcher.launch(REQUESTED_PERMISSIONS)
             }
         }
     }
